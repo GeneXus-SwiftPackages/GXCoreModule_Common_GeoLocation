3057769ae9ae632a3004ce4f93b23ec51e124752
genexusbot <builder@genexus.com>
2023-07-29 18:44:08 -0300

    Package version: 1.1.0-rc.1

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXCoreModule_Common_GeoLocation",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXCoreModule_Common_GeoLocation",
+			targets: ["GXCoreModule_Common_GeoLocationWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0-rc.1"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0-rc.1"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "1.1.0-rc.1"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.1.0-rc.1"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "1.1.0-rc.1"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXStandardClasses.git", exact: "1.1.0-rc.1")
+	],
+	targets: [
+		.target(name: "GXCoreModule_Common_GeoLocationWrapper",
+				dependencies: [
+					"GXCoreModule_Common_GeoLocation",
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.watchOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .watchOS, .iOS])),
+					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.watchOS])),
+					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.watchOS])),
+					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.watchOS])),
+					.product(name: "GXStandardClasses", package: "GXStandardClasses", condition: .when(platforms: [.watchOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXCoreModule_Common_GeoLocation",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_Common_GeoLocation-1.1.0-rc.1.xcframework.zip",
+			checksum: "4fc64a3cc9cb120f1f54f5c3801054d25e923e61f9d358971c12b6f907f70124"
+		)
+	]
+)
