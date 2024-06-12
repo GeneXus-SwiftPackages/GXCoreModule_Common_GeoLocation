e13a0a64457ba641e521ec54e9d54479dc52014f
Build Er <builder@macmini6.local>
2024-06-12 16:22:32 -0300

    Package version: 2.1.0-beta.23

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_Common_GeoLocationWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.41")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.1.0-beta.23")
 	],
 	targets: [
 		.target(name: "GXCoreModule_Common_GeoLocationWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_Common_GeoLocation",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_Common_GeoLocation-2.0.0-beta.41.xcframework.zip",
-			checksum: "336125d4c03fbe26bddca92054caee8078a01cc561fc936eeb16feb3d9d480c6"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_Common_GeoLocation-2.1.0-beta.23.xcframework.zip",
+			checksum: "10ead11f34ffa2eb2d423f393962d3c903767cd82aeac26d23914ce95d668be3"
 		)
 	]
 )
